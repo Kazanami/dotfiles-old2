cd0493ed7e70293a34afedbcfc356db6a9765e37
Kazanami@Ubuntu <dummy.github.1945@gmail.com>
2020-05-22 00:08:24 +0900

    added and run fish installer

--- bin/scripts/fish.sh
+++ bin/scripts/fish.sh
@@ -1,21 +1,12 @@
 #!/bin/bash -xeu
 echo -n "Setup fish shell..."
+sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt update > /dev/null 2>&1
-sudo apt install -y neovim 2>&1
+sudo apt install -y fish > /dev/null 2>&1
+tcsh -s /usr/bin/fish
 if [[ $? == "0" ]];then
 	echo "[ Done ]"
 else
 	echo "[ FAIL ]"
 	exit 33
-fi
-
-echo "Linking..."
-ln -s ${DOTFILES}/nvim ~/.config/
-
-echo "Downloading Plugin Manager..."
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-if [[ -f "~/.config/nvim/autoload/plug.vim" ]];then
-	echo "Plugin Manager Installed"
-	exit 0
 fi
