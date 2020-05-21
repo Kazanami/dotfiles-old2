ca7b08dd0e8bec00a08a933e193d91820ab5e62b
Kazanami@Ubuntu <dummy.github.1945@gmail.com>
2020-05-22 00:18:30 +0900

    Github action error fixed

--- bin/scripts/fish.sh
+++ bin/scripts/fish.sh
@@ -3,7 +3,8 @@ echo -n "Setup fish shell..."
 sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt update > /dev/null 2>&1
 sudo apt install -y fish > /dev/null 2>&1
-chsh -s /usr/bin/fish
+#chsh -s /usr/bin/fish
+
 if [[ $? == "0" ]];then
 	echo "[ Done ]"
 else
