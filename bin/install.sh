b2e14154d7a3fe234db8e211f25349efe17e4948
Kazanami@Ubuntu <dummy.github.1945@gmail.com>
2020-05-22 00:11:36 +0900

    fix typo
    main installer added

---
+++ bin/install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -exu
+#cat dotfiles_logo.txt
+DOTFILES_GITHUB="https://github.com/Kazanami/dotfiles.git"
+DOTFILES_PATH="~/dotfiles"
+dotfiles_logo='
+Welcome 
+            _       _    __ _ _           
+           | |     | |  / _(_) |          
+         __| | ___ | |_| |_ _| | ___  ___ 
+        / _` |/ _ \| __|  _| | |/ _ \/ __|
+       | (_| | (_) | |_| | | | |  __/\__ \
+        \__,_|\___/ \__|_| |_|_|\___||___/
+					Setup!!!
+
+  Licensed under the MIT license.
+'
+echo "${dotfiles_logo}"
+
+echo "Checking Package update...";
+sudo apt update
+
+echo "Install the Required Packages..."
+sudo apt install git curl
+
+if [[ $? == "0" ]];then
+	echo "Complited."
+else 
+	echo "FAIL!!"
+	exit 1
+fi
+
+if [[ -n -d "~/dotfiles" ]];then
+	echo "Cloning Kazanami Dotfiles..."
+	git clone --recursive ${DOTFILES_GITHUB} "${DOTFILES_PATH}"
+fi
+
+cd ${DOTFILES_PATH}
+
+echo "Installing adding Tools..."
+# Install neovim
+./scripts/nvim.sh
+
+# Install fish
+./scripts/fish.sh
--- bin/scripts/fish.sh
+++ bin/scripts/fish.sh
@@ -3,7 +3,7 @@ echo -n "Setup fish shell..."
 sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt update > /dev/null 2>&1
 sudo apt install -y fish > /dev/null 2>&1
-tcsh -s /usr/bin/fish
+chsh -s /usr/bin/fish
 if [[ $? == "0" ]];then
 	echo "[ Done ]"
 else
