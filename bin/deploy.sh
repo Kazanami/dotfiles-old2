0b5ef271cf6b961c11978fdba0e572842025e6da
f0rk <dummy.github.1945@gmail.com>
2020-05-22 11:16:45 +0900

    testing

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,6 +7,8 @@ echo "Downloading nVim Plugin Manager..."
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -LSso ~/.config/nvim/autoload/pathogen.vim \
+    https://tpo.pe/pathogen.vim
 if [[ -f "~/.config/nvim/autoload/plug.vim" ]];then
 	nvim +slient +VimEnter +PlugInstall +qall
 	echo "Plugin Manager Installed"
