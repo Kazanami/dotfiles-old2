246966181b0c8a5d227b7811579549e4ee21ff9b
f0rk <dummy.github.1945@gmail.com>
2020-05-22 10:50:19 +0900

    test nodebrew

--- bin/doctor.sh
+++ bin/doctor.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -eu
 echo "Testing Nodebrew..."
 source $HOME/.bashrc
+which nodebrew
+if [ $? == "1" ];then
+  echo "Not found. Canceling..."
+  exit 0
+fi
 
 nodebrew help
-if [ $? == "0" ];
+if [ $? == "0" ];then
   echo "Nodebrew is enable"
 fi
